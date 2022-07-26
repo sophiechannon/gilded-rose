edc1d8b625a7553b03978f5dffea8b030cc4ded9
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-26 15:01:00 +0100

    read me updated and comments added to code to explain item behaviour and legacy code

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 Gilded Rose
 ===========
+
+- Systems need to be able to accept a new class of item (conjured)
+
+Normal items:
+- degrade at 1 a day but can't go below 0
+- items degree at 2 a day when passed their sell-by date
+- never over 50
+
+Aged brie:
+- increases in value by 1
+- never over 50
+- continues improving after SellIn date
+
+Backstage pass:
+- increases in value by 1
+- by 2 within 10 days, and by 3 within 5 days
+- drops to 0 after concert (end of SellIn day)
+- never over 50
+
+Sulfuras:
+- Never have to be sold (infiniate SellIn date)
+- Never degrade in quality
+- never over 50
+
+NEW - conjured:
+- degrades by 2 each day
+- degrades by 4 after SellIn date
--- spec/lib/gilded_rose.rb
+++ spec/lib/gilded_rose.rb
@@ -7,20 +7,24 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        # normal items - degrading by 1 until SellIn date
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
         end
       else
+        # brie and passes increase in quality up to 50
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            # passes fewer than 10 days increase value by 2
             if item.sell_in < 11
               if item.quality < 50
                 item.quality = item.quality + 1
               end
             end
+            # passes fewer than 5 days increase value by 3
             if item.sell_in < 6
               if item.quality < 50
                 item.quality = item.quality + 1
@@ -29,9 +33,11 @@ class GildedRose
           end
         end
       end
+      # except for hand of ragnaros, decrease SellIn every day
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+      # reduce normal item quality by an additional 1 after sell by date
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -41,9 +47,11 @@ class GildedRose
               end
             end
           else
+            # passes loose all value after sellby date
             item.quality = item.quality - item.quality
           end
         else
+          # brie continues to get better by 1
           if item.quality < 50
             item.quality = item.quality + 1
           end
@@ -52,6 +60,8 @@ class GildedRose
     end
   end
 end
+
+# can't touch this
 
 class Item
   attr_accessor :name, :sell_in, :quality
