e337e18e89e82493c44fc7eb0e50e0634b6bc8ca
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-27 11:40:52 +0100

    started TDD process to decrease quality on items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -14,58 +14,59 @@ class GildedRose
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        # normal items - degrading by 1 until SellIn date
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        # brie and passes increase in quality up to 50
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            # passes fewer than 10 days increase value by 2
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            # passes fewer than 5 days increase value by 3
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      # except for hand of ragnaros, decrease SellIn every day
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      # reduce normal item quality by an additional 1 after sell by date
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            # passes lose all value after sellby date
-            item.quality = item.quality - item.quality
-          end
-        else
-          # brie continues to get better by 2 afer expiry
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+    @items[0].quality -= 1
+  #   @items.each do |item|
+  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #       # normal items - degrading by 1 until SellIn date
+  #       if item.quality > 0
+  #         if item.name != "Sulfuras, Hand of Ragnaros"
+  #           item.quality = item.quality - 1
+  #         end
+  #       end
+  #     else
+  #       # brie and passes increase in quality up to 50
+  #       if item.quality < 50
+  #         item.quality = item.quality + 1
+  #         if item.name == "Backstage passes to a TAFKAL80ETC concert"
+  #           # passes fewer than 10 days increase value by 2
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #           # passes fewer than 5 days increase value by 3
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #     # except for hand of ragnaros, decrease SellIn every day
+  #     if item.name != "Sulfuras, Hand of Ragnaros"
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #     # reduce normal item quality by an additional 1 after sell by date
+  #     if item.sell_in < 0
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras, Hand of Ragnaros"
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           # passes lose all value after sellby date
+  #           item.quality = item.quality - item.quality
+  #         end
+  #       else
+  #         # brie continues to get better by 2 afer expiry
+  #         if item.quality < 50
+  #           item.quality = item.quality + 1
+  #         end
+  #       end
+  #     end
+  #   end
   end
 end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -9,90 +9,106 @@ describe GildedRose do
     gilded_rose.update_quality
     expect(item.quality).to eq 49
   end
-  it "decreases the sell_in time by 1 on normal items" do
-    item = Item.new("Shrunken head", 10, 50)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.sell_in).to eq 9
-  end
-  it "increases the quality by 1 on aged brie items before expiry date is reached" do
-    item = Item.new("Aged Brie", 10, 30)
+  it "decreases the quality by 1 on normal items before expiry date is reached - lower quality" do
+    item = Item.new("Shrunken head", 10, 32)
     gilded_rose = GildedRose.new
     gilded_rose.add(item)
     gilded_rose.update_quality
     expect(item.quality).to eq 31
   end
-  it "increases the quality by 1 on aged brie items after expiry date is reached" do
-    item = Item.new("Aged Brie", -1, 30)
+  it "decreases the quality by 1 on normal items before expiry date is reached - multiple items" do
+    item = Item.new("Shrunken head", 10, 32)
+    item_2 = Item.new("Mermaid tail", 5, 21)
     gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.quality).to eq 32
-  end
-  it "decreases the sell_in time by 1 on Aged Brie items" do
-    item = Item.new("Aged Brie", 10, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.sell_in).to eq 9
-  end
-  it "increases the quality by 1 on backstage pass items up until 10 days before the concert" do
-    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
+    gilded_rose.add([item, item_2])
     gilded_rose.update_quality
     expect(item.quality).to eq 31
+    expect(item_2.quality).to eq 20
   end
-  it "increases the quality by 2 on backstage pass items between 10 - 6 days before the concert" do
-    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.quality).to eq 32
-  end
-  it "increases the quality by 2 on backstage pass items between 5 - 1 days before the concert" do
-    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.quality).to eq 33
-  end
-  it "decreases the sell_in time by 1 on backstage pass items" do
-    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.sell_in).to eq 9
-  end
-  it "never decreases the value on Sulfura items" do
-    item = Item.new("Sulfuras, Hand of Ragnaros", 10, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.quality).to eq 30
-  end
-  it "sulfuras expiry date never decreases" do
-    item = Item.new("Sulfuras, Hand of Ragnaros", 10, 30)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.sell_in).to eq 10
-  end
-  it "Aged Brie items cannot exceed quality of 50" do
-    item = Item.new("Aged Brie", 10, 50)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.sell_in).to eq 9
-    expect(item.quality).to eq 50
-  end
-  it "backstage pass items cannot exceed quality of 50" do
-    item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)
-    gilded_rose = GildedRose.new
-    gilded_rose.add(item)
-    gilded_rose.update_quality
-    expect(item.sell_in).to eq 9
-    expect(item.quality).to eq 50
-  end
+  # it "decreases the sell_in time by 1 on normal items" do
+  #   item = Item.new("Shrunken head", 10, 50)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.sell_in).to eq 9
+  # end
+  # it "increases the quality by 1 on aged brie items before expiry date is reached" do
+  #   item = Item.new("Aged Brie", 10, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.quality).to eq 31
+  # end
+  # it "increases the quality by 1 on aged brie items after expiry date is reached" do
+  #   item = Item.new("Aged Brie", -1, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.quality).to eq 32
+  # end
+  # it "decreases the sell_in time by 1 on Aged Brie items" do
+  #   item = Item.new("Aged Brie", 10, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.sell_in).to eq 9
+  # end
+  # it "increases the quality by 1 on backstage pass items up until 10 days before the concert" do
+  #   item = Item.new("Backstage passes to a TAFKAL80ETC concert", 12, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.quality).to eq 31
+  # end
+  # it "increases the quality by 2 on backstage pass items between 10 - 6 days before the concert" do
+  #   item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.quality).to eq 32
+  # end
+  # it "increases the quality by 2 on backstage pass items between 5 - 1 days before the concert" do
+  #   item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.quality).to eq 33
+  # end
+  # it "decreases the sell_in time by 1 on backstage pass items" do
+  #   item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.sell_in).to eq 9
+  # end
+  # it "never decreases the value on Sulfura items" do
+  #   item = Item.new("Sulfuras, Hand of Ragnaros", 10, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.quality).to eq 30
+  # end
+  # it "sulfuras expiry date never decreases" do
+  #   item = Item.new("Sulfuras, Hand of Ragnaros", 10, 30)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.sell_in).to eq 10
+  # end
+  # it "Aged Brie items cannot exceed quality of 50" do
+  #   item = Item.new("Aged Brie", 10, 50)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.sell_in).to eq 9
+  #   expect(item.quality).to eq 50
+  # end
+  # it "backstage pass items cannot exceed quality of 50" do
+  #   item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)
+  #   gilded_rose = GildedRose.new
+  #   gilded_rose.add(item)
+  #   gilded_rose.update_quality
+  #   expect(item.sell_in).to eq 9
+  #   expect(item.quality).to eq 50
+  # end
 end
