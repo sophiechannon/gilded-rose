0a91b458accd34252f868e8901e21da5b7b662d1
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-07-27 12:00:08 +0100

    first test passing for aged brie

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -48,13 +48,13 @@ describe GildedRose do
     expect(item.sell_in).to eq 9
     expect(item_2.sell_in).to eq 4
   end
-  # it "increases the quality by 1 on aged brie items before expiry date is reached" do
-  #   item = Item.new("Aged Brie", 10, 30)
-  #   gilded_rose = GildedRose.new
-  #   gilded_rose.add(item)
-  #   gilded_rose.update_quality
-  #   expect(item.quality).to eq 31
-  # end
+  it "increases the quality by 1 on aged brie items before expiry date is reached" do
+    item = Item.new("Aged Brie", 10, 30)
+    gilded_rose = GildedRose.new
+    gilded_rose.add(item)
+    gilded_rose.update_quality
+    expect(item.quality).to eq 31
+  end
   # it "increases the quality by 1 on aged brie items after expiry date is reached" do
   #   item = Item.new("Aged Brie", -1, 30)
   #   gilded_rose = GildedRose.new
